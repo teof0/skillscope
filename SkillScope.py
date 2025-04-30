1790ae6a9b3d1a9c800b9ba88ca1033011e68117
teof0 <154710139+teof0@users.noreply.github.com>
2025-04-30 11:24:22 -0400

    Update SkillScope.py

--- SkillScope.py
+++ SkillScope.py
@@ -8,7 +8,7 @@ import numpy as np
 # Set wide layout for dashboard
 st.set_page_config(layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>Welcome to SkillScope!</h1>", unsafe_allow_html=True)  # PAGE TITLE
+st.title("Welcome to SkillScope!")  # PAGE TITLE
 image = Image.open("logo.png")  # OPEN IMAGE
 st.image(image, width=400)  # DISPLAY IMAGE
 
